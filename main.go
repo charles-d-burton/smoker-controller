ad48250dd09f9c8a032b1ee8ead200cde90e9ae9
charles-d-burton <charles.burton@mindflash.com>
2018-09-25 20:20:50 -0600

    added mqtt to golang

--- main.go
+++ main.go
@@ -3,26 +3,147 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"fmt"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
 )
+
+var (
+	subTopics = []string{
+		"/cooktroller/charles/smoker/set_temp",
+		"/cooktroller/charles/smoker/runstate",
+	}
+	serverState ServerState
+)
+
+//Server state and tuning parameters
+type ServerState struct {
+	KP      float32
+	KI      float32
+	KD      float32
+	Loop    int16
+	Running bool
+	TempSet float32
+}
+
+//Server status message
+type ServerStatus struct {
+	DeviceID string  `json:"device_id"`
+	Running  bool    `json:"running"`
+	TempCel  float32 `json:"temp_cel"`
+	TempFar  float32 `json:"temp_far"`
+}
+
+//SetTemperature unmarshal json to set temp
+type SetTemperature struct {
+	TempFar float32 `json:"temp_far"`
+	TempCel float32 `json:"temp_cel"`
+}
+
+//SetRunningState unmarshal json to set run state
+type SetRunningState struct {
+	Running bool `json:"on"`
+}
 
 func main() {
 	log.Println("Hello world")
+
 }
 
-func connect() {
-  cid := uuid.New().String()
-  connOpts := MQTT.NewClientOptions()
-  connOpts.SetClientID(cid)
-  connOpts.SetCleanSession(true)
-  connOpts.SetAutoReconnect(true)
-  connOpts.SetMaxReconnectInterval(1* time.Second)
-  connOpts.SetTLSConfig(getTLSConfig())
-  #mqttClient, err := 
+//Connect to and setup MQTT
+func connect() error {
+	cid := uuid.New().String()
+	connOpts := MQTT.NewClientOptions()
+	connOpts.SetClientID(cid)
+	connOpts.SetCleanSession(true)
+	connOpts.SetAutoReconnect(true)
+	connOpts.SetMaxReconnectInterval(1 * time.Second)
+	connOpts.SetTLSConfig(getTLSConfig())
+	brokerURL := fmt.Sprintf("tcps://%s:%d%s", "a10cp24047duti.iot.us-east-1.amazonaws.com", 8883, "/")
+	connOpts.AddBroker(brokerURL)
+	mqttClient := MQTT.NewClient(connOpts)
+	token := mqttClient.Connect()
+	token.WaitTimeout(30 * time.Second)
+	token.Wait()
+	if token.Error() != nil {
+		log.Println(token.Error())
+		return token.Error()
+	}
+
+	for _, topic := range subTopics {
+		token := mqttClient.Subscribe(topic, 1, subscriber)
+		token.WaitTimeout(30 * time.Second)
+		token.Wait()
+		if token.Error() != nil {
+			return token.Error()
+		}
+	}
+	//Start the PID control loop
+	pidControlLoop(10*time.Second, doControl, mqttClient)
+	return nil
 }
 
+//Function to subscribe to a topic
+func subscriber(client MQTT.Client, msg MQTT.Message) {
+	log.Println("Message", string(msg.Payload()))
+	if msg.Topic() == subTopics[0] {
+		var setTemp SetTemperature
+		err := json.Unmarshal(msg.Payload(), setTemp)
+		if err != nil {
+			log.Println("Unable to unmarshal temp set")
+			return
+		}
+		serverState.TempSet = setTemp.TempFar //set the temperature (F for now)
+	} else if msg.Topic() == subTopics[1] {
+		var setRunState SetRunningState
+		err := json.Unmarshal(msg.Payload(), setRunState)
+		if err != nil {
+			log.Println("Unable to unmarshal set running state")
+			return
+		}
+		serverState.Running = setRunState.Running //set opcode running
+	} else {
+		log.Println("Message from unknown topic")
+	}
+
+}
+
+//Enter a loop to run the PID controller on a schedule
+func pidControlLoop(d time.Duration, f func(time.Time), mqttClient MQTT.Client) {
+	for x := range time.Tick(d) {
+		log.Println("Running control loop")
+		go f(x)
+		var status ServerStatus
+		go status.sendStatus(mqttClient)
+	}
+}
+
+//TODO: Perform the PID Control
+func doControl(t time.Time) {
+	log.Println("running control")
+}
+
+//Set the status on the struct and send it to the MQTT backend
+func (status *ServerStatus) sendStatus(mqttClient MQTT.Client) {
+	status.DeviceID = "Cooktroller Smoker"
+	status.Running = serverState.Running
+	status.TempFar = serverState.TempSet
+	data, err := json.Marshal(status)
+	if err != nil {
+		log.Println(err)
+	}
+	token := mqttClient.Publish("/cooktroller/charles/smoker/status", 1, false, data)
+	token.WaitTimeout(30 * time.Second)
+	token.Wait()
+	if token.Error() != nil {
+		log.Println("Unable to send message to MQTT", token.Error())
+	}
+}
 
 //NewTlsConfig Load in the certificates and setup the TLS configurations and certs
 func getTLSConfig() *tls.Config {
